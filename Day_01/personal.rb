89c37e446783ee69f7dc07a4845a372b7badb23b
My-Boy001 <nitin.soni@kalvium.community>
2025-05-21 14:34:55 +0530

    Create hello.rb and personal.rb with ruby

---
+++ Day_01/personal.rb
@@ -0,0 +1,13 @@
+name = "Nitin Soni"
+age = 20
+college = "Jecrc University"
+current_course = "Software Product Engineering"
+year = "2nd year"
+hobby = "Playing Cricket"
+city = "Jaipur"
+
+
+puts "Hello, my name is #{name}."
+puts "I am #{age} years old and currently studying #{current_course} in my #{year} at #{college}."
+puts "I live in #{city} and enjoy #{hobby}."
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Ruby_On_Rails
+
+
+
